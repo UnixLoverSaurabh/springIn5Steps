f4609d9bdacd34771c4e265752f0fc5b75c56d4a
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-05-18 10:37:40 +0530

    set up unit testing environment in intellij with JUnit

---
+++ UnitTestingWithJUnit/src/main/unixlover/junit/MyMath.java
@@ -0,0 +1,8 @@
+package main.unixlover.junit;
+
+public class MyMath {
+
+    public static void main(String[] args) {
+        System.out.println("Hi there");
+    }
+}
---
+++ UnitTestingWithJUnit/test/main/unixlover/junit/MyMathTest.java
@@ -0,0 +1,11 @@
+package main.unixlover.junit;
+
+import org.junit.Test;
+
+public class MyMathTest {
+
+    @Test
+    public void test() {
+        System.out.println("Test passed");
+    }
+}
