9aefd7b29aa5691fa9f29c6a676cb5aae53961fe
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-05-18 10:49:25 +0530

    succesfully executed test of sum function and checked it using assertEquals()

--- UnitTestingWithJUnit/src/main/unixlover/junit/MyMath.java
+++ UnitTestingWithJUnit/src/main/unixlover/junit/MyMath.java
@@ -2,6 +2,14 @@ package main.unixlover.junit;
 
 public class MyMath {
 
+    int sum(int[] numbers) {
+        int summ = 0;
+        for(int i : numbers) {
+            summ += i;
+        }
+        return summ;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hi there");
     }
--- UnitTestingWithJUnit/test/main/unixlover/junit/MyMathTest.java
+++ UnitTestingWithJUnit/test/main/unixlover/junit/MyMathTest.java
@@ -2,10 +2,17 @@ package main.unixlover.junit;
 
 import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyMathTest {
 
     @Test
     public void test() {
-        System.out.println("Test passed");
+
+        MyMath myMath = new MyMath();
+        int result = myMath.sum(new int[]{1, 2, 3});
+
+        // Check the result is 6
+        assertEquals(6, result);
     }
 }
