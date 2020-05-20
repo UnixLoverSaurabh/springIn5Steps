78b44b06e9d4a6296a28578b0d3e73b54e2270ed
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-05-20 18:14:12 +0530

    simple modification for JSP -- http://localhost:8080/login.do

---
+++ WebMaven/src/main/java/webapp/LoginJsp.java
@@ -0,0 +1,17 @@
+package webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginJsp extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        httpServletRequest.getRequestDispatcher("WEB-INF/views/login.jsp").forward(httpServletRequest, httpServletResponse);
+    }
+}
